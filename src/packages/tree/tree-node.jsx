75a73febdede09ca29bc42565e80e1cd535fe77e
ewall <1054064180@qq.com>
2021-02-22 23:40:28 +0800

    update：tree-node component

--- src/packages/tree/tree-node.jsx
+++ src/packages/tree/tree-node.jsx
@@ -1,3 +1,5 @@
+import { computed } from 'vue'
+
 export default {
   name: 'PanTreeNode',
   props: {
@@ -7,11 +9,22 @@ export default {
     }
   },
   setup(props) {
+    const showArrow = computed(() => {
+      return props.data.children && props.data.children.length
+    })
+    const classs = computed(() => [
+      'pan-tree-node',
+      !showArrow.value && 'pan-tree-no-expand'
+    ])
+
     return () => {
       let { data } = props
       return (
-        <div class="pan-tree-node">
-          <div class="pan-tree-label">{data.name}</div>
+        <div class={classs.value}>
+          <div class="pan-tree-label">
+            <pan-icon icon="xianxingbaozhu" />
+            <span>{data.name}</span>
+          </div>
           <div class="pan-tree-list">
             {data.children &&
               data.children.map(child => <pan-tree-node data={child} />)}
